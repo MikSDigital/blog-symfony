e629b77830462715886a2e8cb0c3a09fdd26dbb1
Ahmed TAILOULOUTE <ahmed.tailouloute@gmail.com>
2017-09-20 21:26:03 +0200

    Ignore .idea folder

---
+++ src/DataFixtures/ORM/PostFixtures.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace App\DataFixtures\ORM;
+
+use App\Entity\Post;
+use Doctrine\Bundle\FixturesBundle\Fixture;
+use Doctrine\Common\Persistence\ObjectManager;
+
+class PostFixtures extends Fixture
+{
+    /**
+     * Load data fixtures with the passed EntityManager
+     *
+     * @param ObjectManager $manager
+     */
+    public function load(ObjectManager $manager)
+    {
+        $post = new Post();
+        $post->setPublishedAt(new \DateTime());
+        $post->setSlug('hello-world');
+        $post->setContent(<<<EOL
+# H1
+## H2
+
+Hello world post
+EOL
+);
+
+        $manager->persist($post);
+        $manager->flush();
+    }
+}
